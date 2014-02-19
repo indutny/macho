0f2897cf5ccf55595bbc750d32df7b28d19966b7
Fedor Indutny <fedor.indutny@gmail.com>
2014-02-20 01:04:24 +0400

    initial

---
+++ test/api-test.js
@@ -0,0 +1,15 @@
+var macho = require('..');
+var fs = require('fs');
+var assert = require('assert');
+
+describe('macho', function() {
+  it('should parse node.js binary', function() {
+    var exe = macho.parse(fs.readFileSync(process.execPath));
+    assert(exe.bits === 64 || exe.bits === 32);
+    assert.equal(exe.filetype, 'execute');
+    assert(exe.cmds.some(function(cmd) {
+      return cmd.type === 'version_min_macosx' &&
+             cmd.version === '10.5.0';
+    }));
+  });
+});
